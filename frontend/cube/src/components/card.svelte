f143eb2452a3fe733769ed5cd6a8dbe1a6bd18ac
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-08-16 11:45:30 +0300

    path_to_product added in Card

--- frontend/cube/src/components/card.svelte
+++ frontend/cube/src/components/card.svelte
@@ -5,6 +5,7 @@
     export let price = ''
     export let product_slug = ''
     export let product_id
+    export let path_to_product = '/product/'
 
     let heart = 'm8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z'
     let heart_filled = 'M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z'
@@ -52,7 +53,7 @@
 </script>
 
 <div class="card_cont">
-    <a data-sveltekit-preload-data="tap" href="/products/{product_slug}">
+    <a data-sveltekit-preload-data="tap" href="{path_to_product}{product_slug}">
         <img class="mb-5" src={src} alt={src} />
     </a>
 
--- frontend/cube/src/routes/+page.svelte
+++ frontend/cube/src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import Mygallery from "../components/mygallery.svelte"
     /** @type {import('./$types').PageData} */
     export let data
+    let path_to_product = '/product/'
 
 </script>
 
@@ -15,7 +16,7 @@
     <div class="gallary_wrap">
         <Mygallery img_height='30vw' products={data.results} />
     </div>
-    <h1 class="pt-9 ml-8">Our Products</h1>
+    <h1 class="pt-9">Our Products</h1>
     <div class="cat">
         {#each data.results as product}
             <Card header={product.name} 
@@ -23,7 +24,8 @@
                 price={product.price} 
                 src={product.images[0] ? product.images[0].img_name : ''} 
                 product_id={product.id} 
-                product_slug={product.slug}/>
+                product_slug={product.slug}
+                path_to_product={path_to_product}/>
         {/each}
     </div>
 </div>
@@ -37,6 +39,9 @@
         margin: 0 auto;
         padding: 0 2rem;
         max-width: 90%;
+    }
+    h1 {
+        margin-left: 2rem;
     }
     .cat {
         display: grid;
