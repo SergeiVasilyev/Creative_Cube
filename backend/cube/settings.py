ba28e13b11580bfdfd9a6ebf888e187afde7f0f0
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-08-27 22:31:20 +0300

    Settings SessionAuthentication

--- backend/cube/settings.py
+++ backend/cube/settings.py
@@ -140,6 +140,10 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer', # Toggle Browser interface
