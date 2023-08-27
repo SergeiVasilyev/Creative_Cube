8ec76d970655bb095552041edad126acfb0a1ff2
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-08-27 22:30:47 +0300

    Svelte Login comp

---
+++ frontend/cube/src/routes/Login.svelte
@@ -0,0 +1,31 @@
+<script>
+    let username = "";
+    let password = "";
+  
+    async function login() {
+      const response = await fetch("/api/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+  
+      if (response.ok) {
+        // Redirect or perform other actions on successful login
+      } else {
+        // Handle login error
+      }
+    }
+  </script>
+  
+  <main>
+    <h1>Login</h1>
+    <input type="text" bind:value={username} placeholder="Username" />
+    <input type="password" bind:value={password} placeholder="Password" />
+    <button on:click={login}>Login</button>
+  </main>
+  
+  <style>
+    /* Your styles here */
+  </style>
---
+++ frontend/cube/src/routes/auth/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Login from '../Login.svelte';
+</script>
+
+<div>
+    <h1>Auth page</h1>
+    <Login />
+</div>
